a391d00a552c6a67d4aa6053fe0b9161d9cde762
nlambriICH <nicola.lambri@cancercenter.humanitas.it>
2022-09-05 12:02:49 +0200

    Add AssemblyInfo

--- TMIJunction/Main.cs
+++ TMIJunction/Main.cs
@@ -1,5 +1,4 @@
 using TMIJunction;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using VMS.TPS.Common.Model.API;
@@ -9,12 +8,6 @@ using System;
 using System.Linq;
 using VMS.TPS.Common.Model.Types;
 
-// TODO: Replace the following version attributes by creating AssemblyInfo.cs. You can do this in the properties of the Visual Studio project.
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
-[assembly: AssemblyInformationalVersion("1.0")]
-
-// TODO: Uncomment the following line if the script requires write access.
 [assembly: ESAPIScript(IsWriteable = true)]
 
 namespace VMS.TPS
